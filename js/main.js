1982cc8658303d6b0ecfe25b6d8bd00d7cf638d8
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-06-29 17:36:23 +0100

    addPost, addCategory and addUser modals done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,2 @@
-const date = new Date();
-document.querySelector('.year').innerHTML = date.getFullYear();
-
-setTimeout(function() {
-    $('#message').fadeOut('slow');
-}, 3000);
+// Get the current year for the copyright
+$('#year').text(new Date().getFullYear());
